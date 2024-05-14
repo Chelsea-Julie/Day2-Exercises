5bd5e68958782d5a92c634a8f03936074d5041da
Chelsea-Julie <chelseaqjulie.gmail.com>
2024-05-14 14:25:13 +0200

    activity

---
+++ script/code.js
@@ -0,0 +1,19 @@
+// question1
+let firstName = "Chelsea";
+let lastName = "Julie";
+
+// question2
+let fullName = firstName + " " + lastName;
+
+console.log(fullName)
+
+// question3
+let numberVar = "5"
+
+// question4
+console.log(numberVar * 5)
+
+// question5
+const name = "Chelsea"
+name = "Julie"
+console.log(name)
